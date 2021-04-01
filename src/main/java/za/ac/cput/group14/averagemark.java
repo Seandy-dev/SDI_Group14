84cb39be23e3d4f8711771e273d5c0b6396b0384
Nikitha Mbokotwana <lonambokotwan117@gmail.com>
2021-04-01 23:41:49 +0200

    second commit

--- src/main/java/za/ac/cput/group14/averagemark.java
+++ src/main/java/za/ac/cput/group14/averagemark.java
@@ -6,4 +6,16 @@ package za.ac.cput.group14;
 
 public class averagemark {
 
+    public static void main(String[] args) {
+
+        System.out.println("Average mark of students");
+
+    }
+    public static double average(double num1, double num2, double num3)
+    {
+        return (num1 + num2 + num3) / 3;
+    }
+
+
+
 }
