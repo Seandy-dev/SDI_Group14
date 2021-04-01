0a19e3bd2d063a4a074a3d29a6ca60d27138d43d
Thina <217217095@mycput.ac.za>
2021-04-01 02:33:07 +0200

    First Commit

---
+++ src/test/java/za/ac/cput/group14/DogTest.java
@@ -0,0 +1,14 @@
+package za.ac.cput.group14;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/*
+ *@Author: Thina Mbiza
+ * Student Number: 217217095
+ * Date: 1 April 2021
+ * This is the test class for the Dog class
+ */
+
+class DogTest {
+
+}
