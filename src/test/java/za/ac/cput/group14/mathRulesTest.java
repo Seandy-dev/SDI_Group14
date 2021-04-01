3cd1dfc34b8ed584efd6ebe57d8493ae7165a9ea
Hlombekazi <hlombekazimbelu@gmail.com>
2021-04-01 16:06:41 +0200

    Fourth commit final commit

--- src/test/java/za/ac/cput/group14/mathRulesTest.java
+++ src/test/java/za/ac/cput/group14/mathRulesTest.java
@@ -5,14 +5,14 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.Test;
-
 import java.util.concurrent.TimeUnit;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class mathRulesTest {
     /**
-     * Rigorous Test :-)
+     * @author 209024666 Hlombekazi Mbelu
+     *
+     * Test method!!
      */
    mathRules maths = new mathRules();
 
@@ -38,20 +38,14 @@ class mathRulesTest {
     @Test
     public  void quantityIdentity()
     {
-        assertSame(6, maths.quantity(2,3,6));
+        assertNotEquals(2,maths.quantity(0,0,1));
     }
 
     @Test
     public  void quantityFail()
     {
-       try
-       {
-           fail("This test is working on my nerves");
-       }catch (final RuntimeException e)
-       {
-           assertTrue(true);
-       }
-       }
+     assertNotSame(2, maths.quantity(2,3,5));
+    }
 
 
     @Test
