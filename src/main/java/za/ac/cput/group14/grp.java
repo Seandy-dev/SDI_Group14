de818c0e2da0779eaab69d833983e1fb01eb16f5
Devon May <219168296@mycput.ac.za>
2021-04-01 10:12:57 +0200

    My First Commit with Complete Assignment

--- src/main/java/za/ac/cput/group14/grp.java
+++ src/main/java/za/ac/cput/group14/grp.java
@@ -1,4 +1,34 @@
 package za.ac.cput.group14;
+//Author Devon May - 219168296
 
-public class grp {
+public class grp
+{
+    private String GroupYear;
+    private String GroupSize;
+
+    public String getGroupSize()
+    {
+
+        return GroupSize;
+    }
+
+    public void setGroupSize(String groupSize) {
+        GroupSize = groupSize;
+    }
+
+    public String getGroupYear() {
+        return GroupYear;
+    }
+
+    public void setGroupYear(String groupYear) {
+        GroupYear = groupYear;
+    }
+
+    @Override
+    public String toString() {
+        return "grp{" +
+                "GroupSize='" + GroupSize + '\'' +
+                ", GroupYear='" + GroupYear + '\'' +
+                '}';
+    }
 }
